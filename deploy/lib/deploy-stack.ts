630c30a2c7ba7750fb4b76e300d7c5c07a74ed33
Brendan Grainger <brendan.grainger@gmail.com>
2023-08-07 17:58:13 -0400

    testing

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,19 +1,33 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from 'aws-cdk-lib'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ecs from 'aws-cdk-lib/aws-ecs'
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns'
+import { Construct } from 'constructs'
 
 export class DeployStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const queue = new sqs.Queue(this, 'DeployQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    const projectName = 'RedWoodBlog'
 
-    const topic = new sns.Topic(this, 'DeployTopic');
+    const vpc = new ec2.Vpc(this, projectName, {
+      maxAzs: 1,
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const cluster = new ecs.Cluster(this, projectName, {
+      vpc: vpc,
+    })
+
+    // eslint-disable-next-line no-new
+    new ecsPatterns.ApplicationLoadBalancedFargateService(this, projectName, {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 1,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true,
+    })
   }
 }
